8fa04b92576bf48c848820410f348067be76a1e6
danilo-mello <dmello19@gmail.com>
2020-11-19 09:57:35 -0800

    Add Data and Item

--- src/main/Model/Data.java
+++ src/main/Model/Data.java
@@ -2,4 +2,28 @@ package main.Model;
 
 public class Data {
 
+    static final String ITEMNAME1 = "Banana";
+    static final String ITEMNAME2 = "Apple";
+    static final String ITEMNAME3 = "Orange";
+    static final String ITEMNAME4 = "Potato";
+    static final String ITEMNAME5 = "Tomato";
+    static final String SMALL = "small";
+    static final String MEDIUM = "medium";
+    static final String LARGE = "large";
+    static final Integer WEIGHTITEM1S = 118;
+    static final Integer WEIGHTITEM1M = 129;
+    static final Integer WEIGHTITEM1L = 140;
+    static final Integer WEIGHTITEM2S = 70;
+    static final Integer WEIGHTITEM2M = 85;
+    static final Integer WEIGHTITEM2L = 100;
+    static final Integer WEIGHTITEM3S = 96;
+    static final Integer WEIGHTITEM3M = 131;
+    static final Integer WEIGHTITEM3L = 184;
+    static final Integer WEIGHTITEM4S = 96;
+    static final Integer WEIGHTITEM4M = 174;
+    static final Integer WEIGHTITEM4L = 286;
+    static final Integer WEIGHTITEM5S = 55;
+    static final Integer WEIGHTITEM5M = 83;
+    static final Integer WEIGHTITEM5L = 142;
+
 }
--- src/main/Model/Item.java
+++ src/main/Model/Item.java
@@ -1,7 +1,61 @@
 package main.Model;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 import java.util.HashMap;
+import java.util.Map;
 
 public class Item {
+
+    public static final Map<String, Integer> GETWEIGHTITEM1;
+    static {
+        GETWEIGHTITEM1 = new HashMap<>();
+        GETWEIGHTITEM1.put(Data.SMALL, Data.WEIGHTITEM1S);
+        GETWEIGHTITEM1.put(Data.MEDIUM, Data.WEIGHTITEM1M);
+        GETWEIGHTITEM1.put(Data.LARGE, Data.WEIGHTITEM1L);
+    }
+
+    public static final Map<String, Integer> GETWEIGHTITEM2;
+    static {
+        GETWEIGHTITEM2 = new HashMap<>();
+        GETWEIGHTITEM2.put(Data.SMALL, Data.WEIGHTITEM2S);
+        GETWEIGHTITEM2.put(Data.MEDIUM, Data.WEIGHTITEM2M);
+        GETWEIGHTITEM2.put(Data.LARGE, Data.WEIGHTITEM2L);
+    }
+
+    public static final Map<String, Integer> GETWEIGHTITEM3;
+    static {
+        GETWEIGHTITEM3 = new HashMap<>();
+        GETWEIGHTITEM3.put(Data.SMALL, Data.WEIGHTITEM3S);
+        GETWEIGHTITEM3.put(Data.MEDIUM, Data.WEIGHTITEM3M);
+        GETWEIGHTITEM3.put(Data.LARGE, Data.WEIGHTITEM3L);
+    }
+
+    public static final Map<String, Integer> GETWEIGHTITEM4;
+    static {
+        GETWEIGHTITEM4 = new HashMap<>();
+        GETWEIGHTITEM4.put(Data.SMALL, Data.WEIGHTITEM4S);
+        GETWEIGHTITEM4.put(Data.MEDIUM, Data.WEIGHTITEM4M);
+        GETWEIGHTITEM4.put(Data.LARGE, Data.WEIGHTITEM4L);
+    }
+
+    public static final Map<String, Integer> GETWEIGHTITEM5;
+    static {
+        GETWEIGHTITEM5 = new HashMap<>();
+        GETWEIGHTITEM5.put(Data.SMALL, Data.WEIGHTITEM5S);
+        GETWEIGHTITEM5.put(Data.MEDIUM, Data.WEIGHTITEM5M);
+        GETWEIGHTITEM5.put(Data.LARGE, Data.WEIGHTITEM5L);
+    }
+
+    public static Map<String,HashMap> Items;
+    static {
+        Items = new HashMap<>();
+        Items.put(Data.ITEMNAME1, (HashMap) GETWEIGHTITEM1);
+        Items.put(Data.ITEMNAME2, (HashMap) GETWEIGHTITEM2);
+        Items.put(Data.ITEMNAME3, (HashMap) GETWEIGHTITEM3);
+        Items.put(Data.ITEMNAME4, (HashMap) GETWEIGHTITEM4);
+        Items.put(Data.ITEMNAME5, (HashMap) GETWEIGHTITEM5);
+    }
+
 
 }
