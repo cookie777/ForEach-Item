853050d6a4b8578377f4ffc8057a729624fc8cc1
Takayuki Yamaguchi <mazic.potion@gmail.com>
2020-11-18 20:42:22 -0800

    add: fundemental user interface

--- src/main/Controller/Controller.java
+++ src/main/Controller/Controller.java
@@ -1,23 +1,48 @@
 package main.Controller;
 
+import java.io.IOException;
 import java.util.Arrays;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.TilePane;
+import javax.xml.soap.Text;
+import main.Model.Conversion;
 
 public class Controller {
+
+
+  @FXML
+  private ListView<String> listView =  new ListView();
+  private ObservableList<String> itemData = FXCollections.observableArrayList();
 
   @FXML
   private Label itemLabel = new Label();
 
   @FXML
-  private ListView<String> listView =  new ListView();
+  private TextField priceFiled = new TextField();
 
-  private ObservableList<String> itemData = FXCollections.observableArrayList();
+  @FXML
+  private TilePane sizeButton = new TilePane();
+
+  @FXML
+  private Button calculateButton = new Button();
+  @FXML
+  private ToggleGroup group = new ToggleGroup();
+
+  @FXML
+  private Label resultValue = new Label();
 
   @FXML
   public void initialize(){
@@ -26,20 +51,71 @@ public class Controller {
     itemData.add("banana");
     itemData.add("orange");
     listView.setItems(itemData);
+
+    itemLabel.setText("Defualt value");
+    priceFiled.setLayoutX(50);
+    priceFiled.setLayoutY(50);
+
+
+//    Label l = new Label("This is a Radiobutton example ");
+    // add label
+//    r.getChildren().add(l);
+    // create radio buttons
+    RadioButton r1 = new RadioButton("Small");
+    RadioButton r2 = new RadioButton("Medium");
+    RadioButton r3 = new RadioButton("Large");
+    r1.setToggleGroup(group);
+    r2.setToggleGroup(group);
+    r3.setToggleGroup(group);
+
+    r1.setSelected(true);
+
+    sizeButton.getChildren().add(r1);
+    sizeButton.getChildren().add(r2);
+    sizeButton.getChildren().add(r3);
+//
+    sizeButton.setLayoutX(50);
+    sizeButton.setLayoutY(100);
+
+    calculateButton.setText("Calculate");
+    calculateButton.setPrefSize(80,40);
+    calculateButton.setLayoutX(50);
+    calculateButton.setLayoutY(150);
+
+    resultValue.setLayoutX(50);
+    resultValue.setLayoutY(200);
+    resultValue.setText("no result");
+
   }
 
   @FXML
-  public void handleMouseClick(MouseEvent arg0) {
+  public void itemClick(MouseEvent arg0) {
     String clickedItemName = listView.getSelectionModel().getSelectedItem();
     System.out.println("click on " + clickedItemName);
-    changeLabel(clickedItemName);
+    itemLabel.setText(clickedItemName);
   }
 
-  public void changeLabel(String newValue){
-    itemLabel.setText(newValue);
+  @FXML
+  public void calculateClick(MouseEvent arg0) {
+    String selectedItem =  itemLabel.getText();
+    System.out.println(selectedItem);
+
+    RadioButton selectedRadioButton = (RadioButton) group.getSelectedToggle();
+    String toogleGroupValue = selectedRadioButton.getText();
+    System.out.println(toogleGroupValue);
+
+    String inputPrice = priceFiled.getText();
+    System.out.println(inputPrice);
+
+    Conversion c = new Conversion();
+    String value = c.test(selectedItem);
+    setResultValue(value);
+
   }
 
-
+  public void setResultValue(String price){
+    resultValue.setText(price);
+  }
 
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,44 +1,22 @@
 package main;
 
-import javafx.scene.input.MouseEvent;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ListView;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent itemList = FXMLLoader.load(getClass().getResource("View/itemList.fxml"));
-        Parent priceConversion = FXMLLoader.load(getClass().getResource("View/itemConversion.fxml"));
+        Parent mainView = FXMLLoader.load(getClass().getResource("View/view.fxml"));
 
-//        StackPane itemListSP = new StackPane();
-//        ListView listView = new ListView();
-//        listView.getItems().add("Item 1");
-//        listView.getItems().add("Item 2");
-//        listView.getItems().add("Item 3");
-//        itemListSP.getChildren().add(listView);
-
-
-        HBox hbox = new HBox(itemList, priceConversion);
-        hbox.getStylesheets().add("main/View/css/styles.css");
+        mainView.getStylesheets().add("main/View/css/styles.css");
         primaryStage.setTitle("Base window");
-        primaryStage.setScene(new Scene(hbox, 848, 480));
+        primaryStage.setScene(new Scene(mainView, 848, 480));
         primaryStage.show();
-
-//        listView.setOnMouseClicked(new EventHandler<MouseEvent>() {
-//            @Override
-//            public void handle(MouseEvent event) {
-//                System.out.println("clicked on " + listView.getSelectionModel().getSelectedItem());
-//            }
-//        });
-
 
     }
 
